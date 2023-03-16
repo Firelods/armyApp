b0cf8b58016c3aebae744dfe673688b9ccd54d46
Clement LEFEVRE <clement.lefevre1@etu.univ-cotedazur.fr>
2023-03-17 00:03:00 +0100

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [loaded] = useFonts({
+    BlackOpsOne: require('./assets/fonts/BlackOpsOne.ttf'),
+  });
+  if (!loaded) {
+    return null;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.textContainer}>
+        <Image style={styles.flagImage} source={require('./assets/drapeau.png')} />
+        <Text style={styles.textPresentation}>Cours pôur votre FMIR ?</Text>
+        <Text style={styles.textPresentation}>Révisions pour vos classes ?</Text>
+        <Text style={styles.textPresentation}>Reçu, vous êtes au bon endroit !</Text></View>
+      <Text>Test !</Text>
     </View>
   );
 }
 
+const backgroundColor = '#324E19';
+const greenBg = "#2B3224"
+const whiteZone = '#E9E9E9'
+
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 5,
+    backgroundColor: greenBg,
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  textContainer: {
+    flex: 3,
+    alignItems: 'center',
+    backgroundColor: whiteZone,
+  },
+  textPresentation: {
+    fontSize: 20,
+    color: backgroundColor,
+    fontFamily: 'BlackOpsOne'
+  },
+  flagImage: {
+    borderBottomLeftRadius: 40,
+    borderBottomRightRadius: 40,
+  }
 });
